2496047def7532862c06ef97eac02628a8f100de
mojroid <dev.moj@gmail.com>
2017-12-30 21:14:03 +0000

    :sparkles:

--- base/src/main/java/moj/memes/base/view/widget/Widget.kt
+++ base/src/main/java/moj/memes/base/view/widget/Widget.kt
@@ -3,7 +3,6 @@ package moj.memes.base.view.widget
 import android.arch.lifecycle.Lifecycle
 import android.arch.lifecycle.LifecycleObserver
 import android.view.View
-import kotlinx.android.extensions.LayoutContainer
 
 interface Widget : LifecycleObserver {
     fun init(lifecycle: Lifecycle, view: View)
