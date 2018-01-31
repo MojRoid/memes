fb7b414946521868aaafa5649a96768701a39ec4
mojroid <dev.moj@gmail.com>
2018-01-31 22:32:16 +0000

    :sparkles:

--- app/src/main/java/moj/memes/app/App.kt
+++ app/src/main/java/moj/memes/app/App.kt
@@ -1,25 +1,22 @@
 package moj.memes.app
 
-import android.app.Activity
-import android.app.Application
 import android.os.StrictMode
 import dagger.android.AndroidInjector
-import dagger.android.DispatchingAndroidInjector
-import dagger.android.HasActivityInjector
+import dagger.android.support.DaggerApplication
 import moj.memes.app.injection.DaggerAppComponent
 import timber.log.Timber
 import javax.inject.Inject
 
-open class App : Application(), HasActivityInjector {
+open class App : DaggerApplication() {
 
     @Inject lateinit var loggingTree: Timber.Tree
-    @Inject lateinit var activityInjector: DispatchingAndroidInjector<Activity>
 
-    override fun activityInjector(): AndroidInjector<Activity> = activityInjector
+    override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
+        return DaggerAppComponent.builder().create(this)
+    }
 
     override fun onCreate() {
         super.onCreate()
-        DaggerAppComponent.builder().application(this).build().inject(this)
         setupDebugTools()
     }
 
--- app/src/main/java/moj/memes/app/injection/AppComponent.kt
+++ app/src/main/java/moj/memes/app/injection/AppComponent.kt
@@ -1,9 +1,8 @@
 package moj.memes.app.injection
 
-import android.app.Application
-import dagger.BindsInstance
 import dagger.Component
-import dagger.android.AndroidInjectionModule
+import dagger.android.AndroidInjector
+import dagger.android.support.AndroidSupportInjectionModule
 import moj.memes.app.App
 import moj.memes.base.injection.BaseModule
 import moj.memes.base.injection.scopes.PerApplication
@@ -11,22 +10,13 @@ import moj.memes.base.network.injection.NetworkModule
 
 @PerApplication
 @Component(modules = [
-    AndroidInjectionModule::class,
+    AndroidSupportInjectionModule::class,
     Bindings::class,
     BaseModule::class,
     NetworkModule::class
 ])
-interface AppComponent {
+interface AppComponent : AndroidInjector<App> {
 
     @Component.Builder
-    interface Builder {
-
-        @BindsInstance
-        fun application(application: Application): Builder
-
-        fun build(): AppComponent
-
-    }
-
-    fun inject(app: App)
+    abstract class Builder : AndroidInjector.Builder<App>()
 }
